bbaf913f1da742ea3fa955ce64a35caf48cfbeff
arunkumar261 <arunkumarreddy261@gmail.com>
2024-05-08 19:51:32 +0530

    updated some code

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,5 +1,6 @@
-bucket = "terraform-remote-state-arundevonline-prod"      #create bucket with this name in aws cloud
-key    = "terraform-multi-env-prod"
+bucket = "roboshop-state-prod"      #create bucket with this name in aws cloud
+key    = "terraform-prod-infra"
 region = "us-east-1"
-dynamodb_table = "terraform-remote-state-arundevonline-lock-prod" #create dunamodb lock with this name in aws cloud
+dynamodb_table = "roboshop-locking-prod" #create dunamodb lock with this name in aws cloud
   
+ 
